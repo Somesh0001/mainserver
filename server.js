48a20b6ffd09eb21db5a3287986d905e92394685
Somesh Dwivedi <sd10072004@gmail.com>
2025-02-14 18:37:58 +0530

    commit_server

---
+++ server.js
@@ -0,0 +1,61 @@
+const http = require("http");
+const server = http.createServer();
+const { Server } = require("socket.io");
+const PORT = process.env.PORT || 4000;
+
+const io = new Server(server, {
+  cors: {
+    origin:"*", 
+  },
+});
+let users = [];
+console.log(users);
+io.on("connection", (socket) => {
+  socket.on("join", (data) => {
+    const user = {
+      socketId: socket.id,
+      name: data.name,
+      coords: { latitude: data.latitude, longitude: data.longitude },
+    };
+    users = users.map((u) => {
+      if (u.socketId === socket.id) {
+        return {
+          socketId: socket.id,
+          name: data.name,
+          coords: { latitude: data.latitude, longitude: data.longitude },
+        };
+      }
+      socket.broadcast.emit("message", users);
+      return u;
+    });
+    if (!users.find((u) => u.socketId === socket.id)) {
+      users.push(user);
+    }
+    socket.broadcast.emit("new-user", user);
+    socket.emit("current-user", user);
+    socket.emit("users", users);
+    socket.emit("message", users);
+  });
+  socket.on("position-change", (data) => {
+    users = users.map((u) => {
+      if (u.socketId === data.socketId) {
+        return data;
+      }
+      console.log(users);
+      socket.emit("message", users);
+      return u;
+    });
+    io.emit("position-change", data);
+    console.log(users);
+  });
+  socket.on("get-data", (data) => {
+    socket.emit("message", users);
+  });
+  socket.on("disconnect", () => {
+    users = users.filter((u) => u.socketId !== socket.id);
+    socket.broadcast.emit("users", users);
+  });
+});
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
